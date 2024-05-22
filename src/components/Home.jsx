886fef8f26553fd668a2b25d9f9edfb95b98ae5e
marleny hoquee <64763342+marlenyhoqueeh@users.noreply.github.com>
2024-05-22 09:26:59 -0500

    Update Home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function Home() {
+    return (
+    <div className='center'><h1 className='black'>Todo lo que buscas a un click!</h1>
+        <button type="button" >Busca aquí</button>
+        <br></br>
+        <br></br>
+        <img src="dresses-1.png" alt="Vestidos de gala en alquiler" />
+    </div>
+    );
+}
+export default Home;
